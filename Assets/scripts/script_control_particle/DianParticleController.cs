658ca00e6b112d3b33a6d2ad20b65c603ec7af99
FiveFireFellow <fivefirefellow@163.com>
2016-07-15 17:52:37 +0800

    dian

---
+++ Assets/scripts/script_control_particle/DianParticleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DianParticleController : MonoBehaviour {
+
+    ParticleSystem _particleSystem;
+    public GameObject parent;
+
+    // Use this for initialization
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (!MagnetismController.trick)
+        {
+            _particleSystem.Stop();
+            //Debug.Log("N stop");
+        }
+        else
+        {
+            _particleSystem.Play();
+            //Debug.Log("N start");
+
+        }
+
+    }
+}
